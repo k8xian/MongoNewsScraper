3f763f6a5026d1e137bda46f54a8978ae0a6b20d
k8xian <kate@innovationsforlearning.org>
2018-10-10 23:56:07 -0500

    re-adding comments now that it's working

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
+//requiring required technologies
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
-
-
-const PORT = process.env.PORT || 3000;
-
-
-const app = express();
 const routes = require("./routes");
 
+//setting up the localhost as 3000 or otherwise deployment config
+const PORT = process.env.PORT || 3000;
+//getting express going
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
+//getting handlebars going
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-
+//using the routes specified in the routes folder
 app.use(routes);
-
-
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-
+//getting mongoose going and specifying collection
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wapoHeadlines";
 mongoose.connect(MONGODB_URI);
 
 
